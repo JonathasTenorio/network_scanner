d28b88c2e09f005539fdabe1c4b2a39a28cc548f
Jonathas Gabriel Davide Tenorio <2009641@aluno.univesp.br>
2024-03-06 17:22:51 -0300

    Scan de top ports

--- network_scanning.sh
+++ network_scanning.sh
@@ -2,26 +2,40 @@
 
 endereco_ip=$(echo $1 | awk -F. '{ print $1"."$2"."$3"." }')
 
-for ip in $(seq 100 103)
+for ip in $(seq 170 172)
 do 
-    resposta=$(ping -c1 $endereco_ip$ip -w1 | grep -i ^64 | awk -F" " '{ print $1 }')
-    if [ ! -z $resposta ]
+    host_up=$(ping -c1 $endereco_ip$ip -w1 | grep -i ^64 | awk -F" " '{ print $1 }')
+    if [ ! -z $host_up ];
     then
         ip_up=($endereco_ip$ip)
-        portas=(22 23 80 443)
+        portas=(1 22 23 80 443)
         for porta in "${portas[@]}"
         do
-            flag=$(sudo hping3 -c 1 --syn -p "$porta" $ip_up 2>&1 | awk -F" "  '{ print $7 }' | awk -F= '{ print $2}')
-            if [[ $flag -eq "RA" ]]
-            then
-                enderecos_up+=( "$ip_up -> Porta $porta")
+            flag_response=$(sudo hping3 -c 1 --syn -p "$porta" $ip_up 2>&1 | grep -i flags)
+            if [[ -z $flag_response ]];
+            then 
+                enderecos_up+=( "$ip_up -> Porta $porta/tcp [ Filtrada ]")
+            else
+                flag=$(echo $flag_response | awk -F" "  '{ print $7 }' | awk -F= '{ print $2}')
+                flag="${flag//$'\n'/}"
+                if [[ "$flag" == "SA" ]];
+                then
+                    enderecos_up+=( "$ip_up -> Porta $porta/tcp [ Aberta ]")
+                elif [[ "$flag" == "RA" ]]
+                then
+                enderecos_up+=( "$ip_up -> Porta $porta/tcp [ Fechada ]")
+                else
+                    enderecos_up+=( "$ip_up -> Porta $porta [ Inacessivel ]")
+                fi
             fi
         done
     fi
 
 done
 
-#echo "${enderecos_up[@]}"
+
+
+
 for up in "${enderecos_up[@]}"
 do
     echo "$up"
