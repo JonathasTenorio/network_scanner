909b35a3457aeb6888ab9d1dad88923f387f33c1
JonathasTenorio <2009641@aluno.univesp.br>
2024-03-06 20:54:25 -0300

    Adicionado funções

--- network_scanning.sh
+++ network_scanning.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
-endereco_ip=$(echo $1 | awk -F. '{ print $1"."$2"."$3"." }')
+clear
 
-for ip in $(seq 170 172)
-do 
-    host_up=$(ping -c1 $endereco_ip$ip -w1 | grep -i ^64 | awk -F" " '{ print $1 }')
-    if [ ! -z $host_up ];
-    then
-        ip_up=($endereco_ip$ip)
-        portas=(1 22 23 80 443)
+varrer_rede(){
+    local endereco_ip=$(echo $1 | awk -F. '{ print $1"."$2"."$3"." }')
+    for ip in $(seq 170 172)
+    do 
+        host_up=$(ping -c1 $endereco_ip$ip -w1 | grep -i ^64 | awk -F" " '{ print $1 }')
+        if [ ! -z $host_up ];
+        then
+            local ip_completo=$endereco_ip$ip
+            scan_portas $ip_completo
+        fi
+    done
+
+}
+
+
+scan_portas(){
+    local ip_ativo=$1
+    portas=(1 22 23 80 443)
         for porta in "${portas[@]}"
         do
-            flag_response=$(sudo hping3 -c 1 --syn -p "$porta" $ip_up 2>&1 | grep -i flags)
+            local flag_response=$(sudo hping3 -c 1 --syn -p "$porta" $ip_ativo 2>&1 | grep -ie "flags" -ie "icmp")
             if [[ -z $flag_response ]];
             then 
-                enderecos_up+=( "$ip_up -> Porta $porta/tcp [ Filtrada ]")
+                enderecos_up+=( "$ip_ativo -> Porta $porta/tcp [ Filtrada ]")
+                echo "${enderecos_up[-1]}"
             else
                 flag=$(echo $flag_response | awk -F" "  '{ print $7 }' | awk -F= '{ print $2}')
                 flag="${flag//$'\n'/}"
                 if [[ "$flag" == "SA" ]];
                 then
-                    enderecos_up+=( "$ip_up -> Porta $porta/tcp [ Aberta ]")
+                    enderecos_up+=( "$ip_ativo -> Porta $porta/tcp [ Aberta ]")
+                    echo "${enderecos_up[-1]}"
                 elif [[ "$flag" == "RA" ]]
                 then
-                enderecos_up+=( "$ip_up -> Porta $porta/tcp [ Fechada ]")
+                    enderecos_up+=( "$ip_ativo -> Porta $porta/tcp [ Fechada ]")
+                    echo "${enderecos_up[-1]}"
                 else
-                    enderecos_up+=( "$ip_up -> Porta $porta [ Inacessivel ]")
+                    enderecos_up+=( "$ip_ativo -> Porta $porta/tcp [ Inacessivel ]")
+                    echo "${enderecos_up[-1]}"
                 fi
             fi
         done
-    fi
-
-done
+}
 
 
 
-
-for up in "${enderecos_up[@]}"
-do
-    echo "$up"
-done
+varrer_rede $1
